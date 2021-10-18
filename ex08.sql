dc2f5c83c1ef0e93798176bde17ce7eb30de9948
Bruno W L Pires <90863493+BrunoWLP@users.noreply.github.com>
2021-10-18 17:04:20 -0300

    Update ex08.sql

--- ex08.sql
+++ ex08.sql
@@ -5,9 +5,9 @@ SELECT order_status
 FROM olist_orders_dataset; -- retorna todas as 99150 linhas de status de pedido
 
 SELECT DISTINCT order_status
-FROM olist_orders_dataset; -- retorna todos os TIPOS de status de pedido
+FROM olist_orders_dataset; -- retorna todos os 8 TIPOS de status de pedido
 
 SELECT order_status as 'status de pedido',
-	   count(order_status) as 'contagem - status de pedido'
+       count(order_status) as 'contagem - status de pedido'
 FROM olist_orders_dataset
 GROUP BY order_status; -- retorna todos os TIPOS e QUANTOS status de pedido há em cada categoria
