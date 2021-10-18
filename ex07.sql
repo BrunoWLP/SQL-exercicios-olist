45573e89ad1d403dfef206ee193e31b72a4d8cf8
Bruno W L Pires <90863493+BrunoWLP@users.noreply.github.com>
2021-10-18 16:56:49 -0300

    Update ex07.sql

--- ex07.sql
+++ ex07.sql
@@ -1,11 +1,10 @@
 /* 7. Crie uma query em SQL que retorne todas as pontuações de avaliação. Utilize a tabela
 “olist_order_reviews_dataset” e a função aliases para retornar o dado.
 */
-SELECT review_score as pontos_avaliação
-FROM olist_order_reviews_dataset;
--- avg(pontos_avaliação) -> Se eu quisesse implementar isso, qual seria a melhor maneira/sintaxe mais limpa?
+SELECT review_score AS pontos_avaliação
+FROM olist_order_reviews_dataset; -- arroz com feijão
 
--- SELECT review_score, 
--- 	count(review_score) as count_review_por_nota
--- FROM olist_order_reviews_dataset
--- GROUP BY review_score;
+SELECT review_score AS 'Pontuação de Avaliação',
+	   count(review_score) AS 'Quantidade de Avaliações'
+FROM olist_order_reviews_dataset
+GROUP BY review_score; -- mais elegante e completo?
