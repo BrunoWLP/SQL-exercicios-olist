8d342e29bfd9fd2187634db4aec1144275475b43
Bruno W L Pires <90863493+BrunoWLP@users.noreply.github.com>
2021-10-18 16:59:36 -0300

    Update ex05.sql

--- ex05.sql
+++ ex05.sql
@@ -1,8 +1,5 @@
 /* 5. Crie uma query em SQL que retorne todos os tipos de pagamento. Utilize a tabela
 “olist_order_payments_dataset” e a função aliases para retornar o dado. 
 */
-SELECT * FROM olist_order_payments_dataset
-LIMIT 1;
-
 SELECT DISTINCT payment_type
 FROM olist_order_payments_dataset;
