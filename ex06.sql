55d6fc5789f170c1e2ded07e2be2fc997aa6579c
Bruno W L Pires <90863493+BrunoWLP@users.noreply.github.com>
2021-10-18 17:02:21 -0300

    Update ex06.sql

--- ex06.sql
+++ ex06.sql
@@ -2,10 +2,10 @@
 compras que foram parceladas de 12 a 24 vezes e que tiveram um valor superior a 245,99 . Utilize
 a tabela “olist_order_payments_dataset” e a função aliases para retornar o dado.
 */
-SELECT * FROM olist_order_payments_dataset
-LIMIT 1;
-
-SELECT payment_type as forma_pagamento, payment_value as valor_pagamento, payment_installments as parcelas
+SELECT payment_type AS forma_pagamento,
+	   payment_value AS valor_pagamento,
+	   payment_installments AS parcelas
 FROM olist_order_payments_dataset
-WHERE (parcelas BETWEEN 12 AND 24) AND (valor_pagamento > 245.99) -- cuidado com vírgula no lugar do ponto!
+WHERE (parcelas BETWEEN 12 AND 24) 
+	AND (valor_pagamento > 245.99) -- cuidar pra não confundir vírgula com ponto!
 ORDER BY parcelas ASC;
